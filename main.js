98c873ae8e141894cd8adf78b14632dc65e27bba
deep <officialdipchandan@gmail.com>
2023-08-02 16:58:54 +0545

    interaction using js

---
+++ main.js
@@ -0,0 +1,25 @@
+// es 5
+var name="dip";
+name="dipchandan";
+
+//es 6
+let rollno=10;
+
+console.log(`Name is ${name} and roll no : ${rollno}`);
+//const
+ const pie=3.14;
+ console.log(pie);
+//  alert("hello");
+
+var name=prompt("enter your name");
+console.log(name);
+let pw=prompt("enter password");
+
+
+
+if (name=="dip" && pw==123){
+    console.log("welcome here");
+}
+else{
+    console.log("enter correct name")
+}
